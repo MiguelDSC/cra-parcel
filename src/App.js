35fdf8da41cd6bc00669ac3a08c6eca9645c282d
Miguel Da Silva Crespim <miguel.da.silva.crespim@hva.nl>
2023-04-12 15:21:11 +0200

    ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import * as Mui from "@mui/icons-material";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        {/* <Mui.GitHub /> */}
       </header>
     </div>
   );
